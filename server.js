df39a34b3a2c91e18bf805ab67b4f37bf74763ed
Harsh <harsh.gupta@cognustechnology.com>
2025-01-22 09:51:04 +0530

    cors and auth chnages

--- server.js
+++ server.js
@@ -6,77 +6,78 @@ import connect from "./db/connect.js";
 import asyncHandler from "express-async-handler";
 import fs from "fs";
 import User from "./models/UserModel.js";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
-  routes: {
-    postLogoutRedirect: process.env.CLIENT_URL,
-    callback: "/callback",
-    logout: "/logout",
-  },
-
-  session: {
-    absoluteDuration: 30 * 24 * 60 * 60 * 1000, // 30 days
-    cookie: {
-      domain: 'localhost',
-      sameSite: "None",
-    },
-  },
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SECRET,
+//   baseURL: process.env.BASE_URL,
+//   clientID: process.env.CLIENT_ID,
+//   issuerBaseURL: process.env.ISSUER_BASE_URL,
+//   routes: {
+//     postLogoutRedirect: process.env.CLIENT_URL,
+//     callback: "/callback",
+//     logout: "/logout",
+//   },
+// };
 app.get("/callback", (req, res) => {
   res.send(`Logged in successfully ${req.oidc.user}`);
 });
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(auth(config));
+// app.use(auth(config));
 
 // function to check if user exists in the db
-const enusureUserInDB = asyncHandler(async (user) => {
-  try {
-    const existingUser = await User.findOne({ auth0Id: user.sub });
+// const enusureUserInDB = asyncHandler(async (user) => {
+//   try {
+//     const existingUser = await User.findOne({ auth0Id: user.sub });
 
-    if (!existingUser) {
-      const newUser = new User({
-        auth0Id: user.sub,
-        email: user.email,
-        name: user.name,
-        role: "jobseeker",
-        profilePicture: user.picture,
-      });
+//     if (!existingUser) {
+//       const newUser = new User({
+//         auth0Id: user.sub,
+//         email: user.email,
+//         name: user.name,
+//         role: "jobseeker",
+//         profilePicture: user.picture,
+//       });
 
-      await newUser.save();
+//       await newUser.save();
 
-      console.log("User added to db", user);
-    } else {
-      console.log("User already exists in db", existingUser);
-    }
-  } catch (error) {
-    console.log("Error checking or adding user to db", error.message);
-  }
-});
+//       console.log("User added to db", user);
+//     } else {
+//       console.log("User already exists in db", existingUser);
+//     }
+//   } catch (error) {
+//     console.log("Error checking or adding user to db", error.message);
+//   }
+// });
 
 app.get("/", async (req, res) => {
-  
-  if (req.oidc.isAuthenticated()) {
-    // check if Auth0 user exists in the db
-    await enusureUserInDB(req.oidc.user);
-    // redirect to the frontend
-    return res.redirect(process.env.CLIENT_URL);
-  } else {
-    return res.send("Logged out");
-  }
+  return res.send("");
+  // if (req.oidc.isAuthenticated()) {
+  //   // check if Auth0 user exists in the db
+  //   await enusureUserInDB(req.oidc.user);
+  //   // redirect to the frontend
+  //   return res.redirect(process.env.CLIENT_URL);
+  // } else {
+  //   return res.send("Logged out");
+  // }
 });
 
 // routes
